d47a0864b02c29b66d9cc6683106f4916d0c0eac
Haqeem Sapuan <haqeemsapuan@gmail.com>
2015-02-07 02:07:22 -0500

    Revert "Revert "Revert "Revert "Revert "Revert "Test""""""
    This reverts commit e4af002273b8e46acb2a1d3b83a1e0a2ba1d6e12.

---
+++ Arduino/libraries/Project/NOT USED/All_Sensors/All_Sensors.ino
@@ -0,0 +1,89 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM303_U.h>
+#include <Adafruit_L3GD20_U.h>
+
+/* Assign a unique ID to this sensor at the same time */
+Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(54321);
+Adafruit_L3GD20_Unified gyro = Adafruit_L3GD20_Unified(20);
+Adafruit_LSM303_Mag_Unified mag = Adafruit_LSM303_Mag_Unified(12345);
+
+
+unsigned long previousMillis = 0; //  unsigned just means it will always be positive
+const long interval = 500; // constant variable, half a second interval
+
+void setup(void) 
+{
+  
+  Serial.begin(9600);
+  
+  // ACCELEROMETER************************************************
+  
+  /* Initialise the sensor */
+  if(!accel.begin())
+  {
+    /* There was a problem detecting the ADXL345 ... check your connections */
+    Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
+    while(1);
+  }
+   
+    
+  // GYRO********************************************
+  
+   /* Enable auto-ranging */
+  gyro.enableAutoRange(true);
+  
+  /* Initialise the sensor */
+  if(!gyro.begin())
+  {
+    /* There was a problem detecting the L3GD20 ... check your connections */
+    Serial.println("Ooops, no L3GD20 detected ... Check your wiring!");
+    while(1);
+  }
+    
+   // MAGNETOMETER****************************
+ 
+  /* Enable auto-gain */
+  mag.enableAutoRange(true);
+  
+  /* Initialise the sensor */
+  if(!mag.begin())
+  {
+    /* There was a problem detecting the LSM303 ... check your connections */
+    Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
+    while(1);
+  }
+ 
+}
+
+void loop(void) 
+{
+  
+   unsigned long currentMillis = millis();
+ 
+  if(currentMillis - previousMillis >= interval) { 
+    previousMillis = currentMillis;  
+    
+  /* Get a new sensor event */ 
+  sensors_event_t event; 
+  accel.getEvent(&event);
+ 
+  /* Display the results (acceleration is measured in m/s^2) */
+  Serial.print ("Accelerometer: ");
+  Serial.print("X: "); Serial.print(event.acceleration.x); Serial.print("  ");
+  Serial.print("Y: "); Serial.print(event.acceleration.y); Serial.print("  ");
+  Serial.print("Z: "); Serial.print(event.acceleration.z); Serial.print("  ");
+  Serial.println("m/s^2 ");
+  
+  Serial.print ("Gyroscope: ");
+  Serial.print("X: "); Serial.print(event.gyro.x); Serial.print("  ");
+  Serial.print("Y: "); Serial.print(event.gyro.y); Serial.print("  ");
+  Serial.print("Z: "); Serial.print(event.gyro.z); Serial.print("  "); 
+  Serial.println("rad/s ");
+  
+  Serial.print ("Magnetometer: ");
+  Serial.print("X: "); Serial.print(event.magnetic.x); Serial.print("  ");
+  Serial.print("Y: "); Serial.print(event.magnetic.y); Serial.print("  ");
+  Serial.print("Z: "); Serial.print(event.magnetic.z); Serial.print("  ");Serial.println("uT");
+  }
+}
